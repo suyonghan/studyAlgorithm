96ad16934af3626e26d5a3789c4310d17617957e
suyong <3519zx@naver.com>
2019-06-24 15:27:53 +0900

    add code

--- programers/src/dfs_bfs/Prob2_Network.java
+++ programers/src/dfs_bfs/Prob2_Network.java
@@ -1,38 +1,53 @@
-package dfs_bfs;
+package sss;
 
 public class Prob2_Network {
-	public static void main(String[] args) {
-		Solution2 s = new Solution2();
-		int numbers[][] = { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } };
-		System.out.println(s.solution(3, numbers));
-		int numbers2[][] = { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } };
-		System.out.println(s.solution(3, numbers2));
-	}
 
 }
 
-class Solution2 {
-	public int solution(int n, int[][] computers) {
-		int answer = 0;
-		boolean[] check = new boolean[computers.length];
-		for (int i = 0; i < computers.length; i++) {
-			if (check[i])
-				continue;
-			dfs(computers, check, i);
-			answer++;
-		}
-		return answer;
-	}
+class Searchs {
+	int n;
+	int answer;
+	boolean[][] isVisit;
 
-	public static void dfs(int[][] a, boolean[] c, int v) {
-		int n = a.length - 1;
-		c[v] = true;
-//		System.out.print(v + " ");
-		for (int i = 1; i <= n; i++) {
-			if (a[v][i] == 1 && !c[i]) {
-				dfs(a, c, i);
+	Searchs(int n, int[][] computers) {
+		this.n = n;
+		answer = 0;
+		isVisit = new boolean[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				if (computers[i][j] == 1)
+					isVisit[i][j] = true;
 			}
 		}
 	}
 
+	// i : 0 ~ n
+	// j : i ~ n
+	void dfs(int depth, int i, int j) {
+		if (depth == 1)
+			answer++;
+		isVisit[i][j] = false;
+
+		if (isVisit[j][j])
+			dfs(depth + 1, j, j);
+		if (j + 1 < n && isVisit[i][j + 1])
+			dfs(depth + 1, i, j + 1);
+	}
+}
+
+class Solutionnetwork {
+	public int solution(int n, int[][] computers) {
+
+		Searchs s = new Searchs(n, computers);
+
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				if (s.isVisit[i][j])
+					s.dfs(1, i, j);
+			}
+		}
+
+		int answer = s.answer;
+		return answer;
+	}
 }
