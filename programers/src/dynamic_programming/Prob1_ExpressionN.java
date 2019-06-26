e71330cac445ccf64c51e1ec8a0796e820d06fa4
한수용 <35483497+suyonghan@users.noreply.github.com>
2019-06-26 13:13:14 +0900

    update code and complete

--- programers/src/dynamic_programming/Prob1_ExpressionN.java
+++ programers/src/dynamic_programming/Prob1_ExpressionN.java
@@ -1,35 +1,52 @@
 package dynamic_programming;
 
-import java.util.HashMap;
-
 public class Prob1_ExpressionN {
 	public static void main(String[] args) {
-
+		Solution s = new Solution();
+		System.out.println(s.solution(5, 12));
+		Solution s1 = new Solution();
+//		System.out.println(s1.solution(2, 11));
+//		Solution s2 = new Solution();
+//		System.out.println(s2.solution(5, 31168));
+//		Solution s3 = new Solution();
+//		System.out.println(s3.solution(5, 24));
+//		Solution s4 = new Solution();
+//		System.out.println(s4.solution(5, 26));
 	}
 }
 
 class Solution {
-	HashMap<String, Integer> hm;
+	int min;
 
 	public Solution() {
-		// TODO Auto-generated constructor stub
-		hm = new HashMap<String, Integer>();
+		min = Integer.MAX_VALUE;
+
 	}
 
 	public int solution(int N, int number) {
-		int answer = 0;
-		if (N == number)
-			return 1;
-		else {
-			int count = 1;
-			hm.put(Integer.toString(N), N);
-			while (count < 10) {
-//				if(!hm.containsKey(key)
+		search(N, number, 0, 0);
+		if (min == Integer.MAX_VALUE)
+			min = -1;
+		return min;
+	}
+
+	public void search(int N, int target, int count, int current) {
+		int temp = N;
+		if (count > 8) {
+			return;
+		} else if (target == current) {
+			this.min = Math.min(min, count);
+			return;
+		} else {
+			for (int i = 0; i < 8 - count; i++) {
+				search(N, target, count + i + 1, current + temp);
+				search(N, target, count + i + 1, current - temp);
+				search(N, target, count + i + 1, current * temp);
+				search(N, target, count + i + 1, current / temp);
+				search(N, target, count + i + 1, N - current);
+				temp = temp * 10 + N;
 			}
-			if (answer > 8)
-				return -1;
-			else
-				return answer;
 		}
 	}
+
 }
