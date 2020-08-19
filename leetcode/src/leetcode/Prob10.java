bd888f6e2b8f0913f9a54073a34c43bfc07a9a67
suyonghan <3519zx@naver.com>
2020-08-19 09:03:01 +0900

    commit Prob10 source(not yet solved)

--- leetcode/src/leetcode/Prob10.java
+++ leetcode/src/leetcode/Prob10.java
@@ -1,5 +1,7 @@
 package leetcode;
 
+import java.util.HashSet;
+
 public class Prob10 {
 	public static void main(String[] args) {
 
@@ -7,12 +9,13 @@ public class Prob10 {
 }
 
 class RegularExpressionMatching {
+	HashSet<String> hs = new HashSet<String>();
 	public boolean isMatch(String s, String p) {
-    	StringBuffer target = new StringBuffer(s);
-    	StringBuffer src = new StringBuffer(p);
+    	String src = s;
+    	String target = p;
     	boolean result = false;
     	
-    	if(!p.contains(".") && !p.contains("*")) {
+    	if(!target.contains(".") && !target.contains("*")) {
     		if(src.length() != target.length())
     			result = false;
     		else if(!src.equals(target))
@@ -21,14 +24,65 @@ class RegularExpressionMatching {
     			result = true;
     	}
     	else {
-    		for(int i = 0; i < target.length(); i++) {
-    			if(target.charAt(i) == src.charAt(i))
-    				continue;
-    			else {
-    				
-    			}
-    		}
+
     	}
     	return result;
     }
+	
+	public void save(int index, String target) {
+		if(index == target.length()) {
+			hs.add(target.toString());
+		}
+		else {
+			for(int i = index; i < target.length(); i++) {
+				if(target.charAt(i) == '*') {
+					StringBuffer temp = new StringBuffer(target);
+					System.out.println("replace 1: " + temp);
+					temp.replace(i, i, target.charAt(i-1)+"");
+					System.out.println("replace 2: " + temp);
+					save(i+1, temp.toString());
+					temp.delete(i-1, i);
+					System.out.println("replace 3: " + temp);
+					save(i+1, temp.toString());
+				}
+			}
+		}
+	}
 }
+
+//'.' Matches any single character.
+//'*' Matches zero or more of the preceding element.
+
+//Example 1:
+//Input:
+//s = "aa"
+//p = "a"
+//Output: false
+//Explanation: "a" does not match the entire string "aa".
+//
+//Example 2:
+//Input:
+//s = "aa"
+//p = "a*"
+//Output: true
+//Explanation: '*' means zero or more of the preceding element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+//
+//Example 3:
+//Input:
+//s = "ab"
+//p = ".*"
+//Output: true
+//Explanation: ".*" means "zero or more (*) of any character (.)".
+//
+//Example 4:
+//Input:
+//s = "aab"
+//p = "c*a*b"
+//Output: true
+//Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore, it matches "aab".
+//
+//Example 5:
+//Input:
+//s = "mississippi"
+//p = "mis*is*p*."
+//Output: false
